1cc43f1346c53f7511959f8cbd6e336aa274e871
Smilla Finschow <smilla.finschow@sap.com>
2026-06-03 14:13:41 +0200

    paramterised test hinzugefügt für Setspürfen & Setsbearbeiten

--- core/src/test/kotlin/hwr/oop/rummikub_2026/core/SetsBearbeitenTest.kt
+++ core/src/test/kotlin/hwr/oop/rummikub_2026/core/SetsBearbeitenTest.kt
@@ -4,208 +4,208 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class SetsBearbeitenTest {
 
-    @Test
-    fun `Hinzufuegen zu 3er Set Eins = gueltiges 4er Set` () {
-        val set = Sets(mutableListOf(
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Rot, Zahl.Eins),
-            Stein(Farbe.Blau, Zahl.Eins)
+    data class SetTestfall(
+        val set: MutableList<Stein>,
+        val stein: Stein,
+        val erwarteteFehlerNachricht: String
+    )
+
+    companion object {
+        @JvmStatic
+        fun gueltigeHinzufuegenZu3erSet() = listOf(
+            // 3er Set Eins + Schwarz Eins
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Eins),
+                    Stein(Farbe.Rot, Zahl.Eins),
+                    Stein(Farbe.Blau, Zahl.Eins)
+                ),
+                Stein(Farbe.Schwarz, Zahl.Eins)
+            ),
+            // 3er Set Sechs + Blau Sechs
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Sechs),
+                    Stein(Farbe.Rot, Zahl.Sechs),
+                    Stein(Farbe.Schwarz, Zahl.Sechs)
+                ),
+                Stein(Farbe.Blau, Zahl.Sechs)
+            ),
+            // 3er Set Dreizehn + Schwarz Dreizehn
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Dreizehn),
+                    Stein(Farbe.Rot, Zahl.Dreizehn),
+                    Stein(Farbe.Blau, Zahl.Dreizehn)
+                ),
+                Stein(Farbe.Schwarz, Zahl.Dreizehn)
+            )
         )
+
+        @JvmStatic
+        fun ungueltigeHinzufuegenZu4erSet() = listOf(
+            // 4er Set Eins + fünfter Stein
+            SetTestfall(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Eins),
+                    Stein(Farbe.Rot, Zahl.Eins),
+                    Stein(Farbe.Blau, Zahl.Eins),
+                    Stein(Farbe.Schwarz, Zahl.Eins)
+                ),
+                Stein(Farbe.Schwarz, Zahl.Eins),
+                "Maximal 4 Steine"
+            ),
+            // 4er Set Sechs + fünfter Stein
+            SetTestfall(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Sechs),
+                    Stein(Farbe.Rot, Zahl.Sechs),
+                    Stein(Farbe.Blau, Zahl.Sechs),
+                    Stein(Farbe.Schwarz, Zahl.Sechs)
+                ),
+                Stein(Farbe.Schwarz, Zahl.Sechs),
+                "Maximal 4 Steine"
+            ),
+            // 4er Set Dreizehn + fünfter Stein
+            SetTestfall(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Dreizehn),
+                    Stein(Farbe.Rot, Zahl.Dreizehn),
+                    Stein(Farbe.Blau, Zahl.Dreizehn),
+                    Stein(Farbe.Schwarz, Zahl.Dreizehn)
+                ),
+                Stein(Farbe.Schwarz, Zahl.Sechs),
+                "Maximal 4 Steine"
+            )
         )
-        //then
-        assertDoesNotThrow {
-            set.hinzufuegenZumSet(Stein(Farbe.Schwarz, Zahl.Eins))
-        }
-        assertThat(Stein(Farbe.Schwarz, Zahl.Eins) in set.setReadOnly).isTrue
+
+        @JvmStatic
+        fun gueltigesWegnehmenVon4erSet() = listOf(
+            // 4er Set Eins - Rot Eins
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Eins),
+                    Stein(Farbe.Rot, Zahl.Eins),
+                    Stein(Farbe.Blau, Zahl.Eins),
+                    Stein(Farbe.Schwarz, Zahl.Eins)
+                ),
+                Stein(Farbe.Rot, Zahl.Eins)
+            ),
+            // 4er Set Sechs - Blau Sechs
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Sechs),
+                    Stein(Farbe.Rot, Zahl.Sechs),
+                    Stein(Farbe.Blau, Zahl.Sechs),
+                    Stein(Farbe.Schwarz, Zahl.Sechs)
+                ),
+                Stein(Farbe.Blau, Zahl.Sechs)
+            ),
+            // 4er Set Dreizehn - Orange Dreizehn
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Dreizehn),
+                    Stein(Farbe.Rot, Zahl.Dreizehn),
+                    Stein(Farbe.Blau, Zahl.Dreizehn),
+                    Stein(Farbe.Schwarz, Zahl.Dreizehn)
+                ),
+                Stein(Farbe.Orange, Zahl.Dreizehn)
+            )
+        )
+
+        @JvmStatic
+        fun ungueltigesWegnehmenVon3erSet() = listOf(
+            // 3er Set Eins - Orange Eins
+            SetTestfall(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Eins),
+                    Stein(Farbe.Rot, Zahl.Eins),
+                    Stein(Farbe.Schwarz, Zahl.Eins)
+                ),
+                Stein(Farbe.Orange, Zahl.Eins),
+                "Mindestens 3 Steine"
+            ),
+            // 3er Set Sechs - Rot Sechs
+            SetTestfall(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Sechs),
+                    Stein(Farbe.Rot, Zahl.Sechs),
+                    Stein(Farbe.Blau, Zahl.Sechs)
+                ),
+                Stein(Farbe.Rot, Zahl.Sechs),
+                "Mindestens 3 Steine"
+            ),
+            // 3er Set Dreizehn - Schwarz Dreizehn
+            SetTestfall(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Dreizehn),
+                    Stein(Farbe.Rot, Zahl.Dreizehn),
+                    Stein(Farbe.Schwarz, Zahl.Dreizehn)
+                ),
+                Stein(Farbe.Schwarz, Zahl.Dreizehn),
+                "Mindestens 3 Steine"
+            )
+        )
     }
 
-    @Test
-    fun `Hinzufuegen zu 3er Set Sechs = gueltiges 4er Set` () {
-        val set = Sets(mutableListOf(
-            Stein(Farbe.Orange, Zahl.Sechs),
-            Stein(Farbe.Rot, Zahl.Sechs),
-            Stein(Farbe.Schwarz, Zahl.Sechs)
-        )
-        )//when
+    @ParameterizedTest
+    @MethodSource("gueltigeHinzufuegenZu3erSet")
+    fun `Hinzufuegen zu 3er Set = gueltiges 4er Set`(
+        testfall: Pair<MutableList<Stein>, Stein>
+    ) {
+        val set = Sets(testfall.first)
+
         assertDoesNotThrow {
-            set.hinzufuegenZumSet(Stein(Farbe.Blau, Zahl.Sechs))
+            set.hinzufuegenZumSet(testfall.second)
         }
-        assertThat(Stein(Farbe.Blau, Zahl.Sechs) in set.setReadOnly).isTrue
+        assertThat(testfall.second in set.setReadOnly).isTrue
     }
 
-    @Test
-    fun `Hinzufuegen zu 3er Set Dreizehn = gueltiges 4er Set` () {
-        val set = Sets(mutableListOf(
-            Stein(Farbe.Orange, Zahl.Dreizehn),
-            Stein(Farbe.Rot, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn),
-        ))
-        //then
-        assertDoesNotThrow {
-            set.hinzufuegenZumSet(Stein(Farbe.Schwarz, Zahl.Dreizehn))
-        }
-        assertThat(Stein(Farbe.Schwarz, Zahl.Eins) in set.setReadOnly)
-    }
+    @ParameterizedTest
+    @MethodSource("ungueltigeHinzufuegenZu4erSet")
+    fun `Hinzufuegen zu 4er Set = ungueltiges Set`(
+        testfall: SetTestfall
+    ) {
+        val set = Sets(testfall.set)
 
-    @Test
-    fun `Hinzufuegen zu 4er Set Eins = ungueltiges Set`() {
-        val set = Sets(mutableListOf(
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Rot, Zahl.Eins),
-            Stein(Farbe.Blau, Zahl.Eins),
-            Stein(Farbe.Schwarz, Zahl.Eins)
-        ))
-
-        //then
         val exception = assertThrows<IllegalArgumentException> {
-            set.hinzufuegenZumSet(Stein(Farbe.Schwarz, Zahl.Eins))
+            set.hinzufuegenZumSet(testfall.stein)
             set.istGueltig()
         }
-        assertThat(exception.message).contains("Maximal 4 Steine")
+        assertThat(exception.message).contains(testfall.erwarteteFehlerNachricht)
     }
 
-    @Test
-    fun `Hinzufuegen zu 4er Set Sechs = ungueltiges Set` () {
-        val set = Sets(mutableListOf(
-            Stein(Farbe.Orange, Zahl.Sechs),
-            Stein(Farbe.Rot, Zahl.Sechs),
-            Stein(Farbe.Blau, Zahl.Sechs),
-            Stein(Farbe.Schwarz, Zahl.Sechs)
-        ))
+    @ParameterizedTest
+    @MethodSource("gueltigesWegnehmenVon4erSet")
+    fun `Wegnehmen vom 4er Set = gueltiges 3er Set`(
+        testfall: Pair<MutableList<Stein>, Stein>
+    ) {
+        val sets = Sets(testfall.first)
 
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            set.hinzufuegenZumSet(Stein(Farbe.Schwarz, Zahl.Sechs))
-            set.istGueltig()
-        }
-        assertThat(exception.message).contains("Maximal 4 Steine")
-    }
-
-    @Test
-    fun `Hinzufuegen zu 4er Set Dreizehn = ungueltiges Set` () {
-        val set = Sets(mutableListOf(
-            Stein(Farbe.Orange, Zahl.Dreizehn),
-            Stein(Farbe.Rot, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn),
-            Stein(Farbe.Schwarz, Zahl.Dreizehn)
-        ))
-
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            set.hinzufuegenZumSet(Stein(Farbe.Schwarz, Zahl.Sechs))
-            set.istGueltig()
-        }
-        assertThat(exception.message).contains("Maximal 4 Steine")
-    }
-
-    @Test
-    fun `Wegnehmen vom 4er Set Eins = gueltiges Set oder gueltige Folge` () {
-
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Rot, Zahl.Eins),
-            Stein(Farbe.Blau, Zahl.Eins),
-            Stein(Farbe.Schwarz, Zahl.Eins)
-        )
-        val sets = Sets(set)
-
-        sets.wegnehmenVomSet(Stein(Farbe.Rot, Zahl.Eins))
+        sets.wegnehmenVomSet(testfall.second)
         sets.istGueltig()
 
-        assertThat(sets.setReadOnly).doesNotContain(Stein(Farbe.Rot, Zahl.Eins))
+        assertThat(sets.setReadOnly).doesNotContain(testfall.second)
     }
 
+    @ParameterizedTest
+    @MethodSource("ungueltigesWegnehmenVon3erSet")
+    fun `Wegnehmen vom 3er Set = ungueltiges Set`(
+        testfall: SetTestfall
+    ) {
+        val sets = Sets(testfall.set)
 
-    @Test
-    fun `Wegnehmen vom 4er Set Sechs = gueltiges Set oder gueltige Folge` () {
-        // given
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Sechs),
-            Stein(Farbe.Rot, Zahl.Sechs),
-            Stein(Farbe.Blau, Zahl.Sechs),
-            Stein(Farbe.Schwarz, Zahl.Sechs)
-        )
-        //when
-        val sets = Sets(set)
-        sets.wegnehmenVomSet(Stein(Farbe.Blau, Zahl.Sechs))
-        sets.istGueltig()
-
-        assertThat(sets.setReadOnly).doesNotContain(Stein(Farbe.Blau, Zahl.Sechs))
-    }
-
-    @Test
-    fun `Wegnehmen vom 4er Set Dreizehn = gueltiges Set oder gueltige Folge` () {
-        // given
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Dreizehn),
-            Stein(Farbe.Rot, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn),
-            Stein(Farbe.Schwarz, Zahl.Dreizehn)
-        )
-        //when
-        val sets = Sets(set)
-
-        sets.wegnehmenVomSet(Stein(Farbe.Orange, Zahl.Dreizehn))
-        sets.istGueltig()
-
-        assertThat(sets.setReadOnly).doesNotContain(Stein(Farbe.Orange, Zahl.Dreizehn))
-    }
-
-    @Test
-    fun `Wegnehmen zu 3er Set Eins = ungueltiges Set` () {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Rot, Zahl.Eins),
-            Stein(Farbe.Schwarz, Zahl.Eins),
-        )
-        val sets = Sets(set)
-
-        //then
         val exception = assertThrows<IllegalArgumentException> {
-            sets.wegnehmenVomSet(Stein(Farbe.Orange, Zahl.Eins))
+            sets.wegnehmenVomSet(testfall.stein)
             sets.istGueltig()
         }
-        assertThat(sets.setReadOnly).doesNotContain(Stein(Farbe.Orange, Zahl.Eins))
-        assertThat(exception.message).contains("Mindestens 3 Steine")
-    }
-
-    @Test
-    fun `Wegnehmen zu 3er Set Sechs = ungueltiges Set` () {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Sechs),
-            Stein(Farbe.Rot, Zahl.Sechs),
-            Stein(Farbe.Blau, Zahl.Sechs),
-        )
-        val sets = Sets(set)
-
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            sets.wegnehmenVomSet(Stein(Farbe.Rot, Zahl.Sechs))
-            sets.istGueltig()
-        }
-        assertThat(sets.setReadOnly).doesNotContain(Stein(Farbe.Rot, Zahl.Sechs))
-        assertThat(exception.message).contains("Mindestens 3 Steine")
-    }
-
-    @Test
-    fun `Wegnehmen zu 3er Set Dreizehn = ungueltiges Set` () {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Dreizehn),
-            Stein(Farbe.Rot, Zahl.Dreizehn),
-            Stein(Farbe.Schwarz, Zahl.Dreizehn)
-        )
-        val sets = Sets(set)
-
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            sets.wegnehmenVomSet(Stein(Farbe.Schwarz, Zahl.Dreizehn))
-            sets.istGueltig()
-        }
-        assertThat(sets.setReadOnly).doesNotContain(Stein(Farbe.Schwarz, Zahl.Dreizehn))
-        assertThat(exception.message).contains("Mindestens 3 Steine")
+        assertThat(sets.setReadOnly).doesNotContain(testfall.stein)
+        assertThat(exception.message).contains(testfall.erwarteteFehlerNachricht)
     }
 
     @Test
--- core/src/test/kotlin/hwr/oop/rummikub_2026/core/SetsPruefenTest.kt
+++ core/src/test/kotlin/hwr/oop/rummikub_2026/core/SetsPruefenTest.kt
@@ -3,242 +3,190 @@ package hwr.oop.rummikub_2026.core
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-
-//import org.junit.jupiter.api.Assertions.assertTrue
-
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class SetsPruefenTest {
-    @Test
-    fun `gueltige Set mit vier Steine, selbe Zahl (Eins) und unterschiedliche Farben`() {
-        // given
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Rot, Zahl.Eins),
-            Stein(Farbe.Blau, Zahl.Eins),
-            Stein(Farbe.Schwarz, Zahl.Eins)
-        )
-        //when
-        val sets = Sets(set)
 
-        //then
-        assertThat(sets.setReadOnly).isEqualTo(set)
+    companion object {
+        @JvmStatic
+        fun gueltigeSets3Steine() = listOf(
+            mutableListOf(
+                Stein(Farbe.Orange, Zahl.Eins),
+                Stein(Farbe.Rot, Zahl.Eins),
+                Stein(Farbe.Blau, Zahl.Eins)
+            ),
+            mutableListOf(
+                Stein(Farbe.Orange, Zahl.Sechs),
+                Stein(Farbe.Blau, Zahl.Sechs),
+                Stein(Farbe.Schwarz, Zahl.Sechs)
+            ),
+            mutableListOf(
+                Stein(Farbe.Rot, Zahl.Dreizehn),
+                Stein(Farbe.Blau, Zahl.Dreizehn),
+                Stein(Farbe.Schwarz, Zahl.Dreizehn)
+            )
+        )
+
+        @JvmStatic
+        fun gueltigeSets4Steine() = listOf(
+            mutableListOf(
+                Stein(Farbe.Orange, Zahl.Eins),
+                Stein(Farbe.Rot, Zahl.Eins),
+                Stein(Farbe.Blau, Zahl.Eins),
+                Stein(Farbe.Schwarz, Zahl.Eins)
+            ),
+            mutableListOf(
+                Stein(Farbe.Orange, Zahl.Sechs),
+                Stein(Farbe.Rot, Zahl.Sechs),
+                Stein(Farbe.Blau, Zahl.Sechs),
+                Stein(Farbe.Schwarz, Zahl.Sechs)
+            ),
+            mutableListOf(
+                Stein(Farbe.Orange, Zahl.Dreizehn),
+                Stein(Farbe.Rot, Zahl.Dreizehn),
+                Stein(Farbe.Blau, Zahl.Dreizehn),
+                Stein(Farbe.Schwarz, Zahl.Dreizehn)
+            )
+        )
+
+        @JvmStatic
+        fun ungueltigeSetsGleicheFarbe() = listOf(
+            // 4 Steine - doppelte Farbe Rot
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Rot, Zahl.Eins),
+                    Stein(Farbe.Rot, Zahl.Eins),
+                    Stein(Farbe.Blau, Zahl.Eins),
+                    Stein(Farbe.Schwarz, Zahl.Eins)
+                ),
+                "Alle Steine muessen unterschiedliche Farbe haben"
+            ),
+            // 4 Steine - doppelte Farbe Orange
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Sechs),
+                    Stein(Farbe.Orange, Zahl.Sechs),
+                    Stein(Farbe.Blau, Zahl.Sechs),
+                    Stein(Farbe.Schwarz, Zahl.Sechs)
+                ),
+                "Alle Steine muessen unterschiedliche Farbe haben"
+            ),
+            // 4 Steine - doppelte Farbe Blau
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Dreizehn),
+                    Stein(Farbe.Blau, Zahl.Dreizehn),
+                    Stein(Farbe.Blau, Zahl.Dreizehn),
+                    Stein(Farbe.Schwarz, Zahl.Dreizehn)
+                ),
+                "Alle Steine muessen unterschiedliche Farbe haben"
+            ),
+            // 3 Steine - doppelte Farbe Orange
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Eins),
+                    Stein(Farbe.Orange, Zahl.Eins),
+                    Stein(Farbe.Blau, Zahl.Eins)
+                ),
+                "Alle Steine muessen unterschiedliche Farbe haben"
+            ),
+            // 3 Steine - doppelte Farbe Schwarz
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Sechs),
+                    Stein(Farbe.Schwarz, Zahl.Sechs),
+                    Stein(Farbe.Schwarz, Zahl.Sechs)
+                ),
+                "Alle Steine muessen unterschiedliche Farbe haben"
+            ),
+            // 3 Steine - doppelte Farbe Blau
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Rot, Zahl.Dreizehn),
+                    Stein(Farbe.Blau, Zahl.Dreizehn),
+                    Stein(Farbe.Blau, Zahl.Dreizehn)
+                ),
+                "Alle Steine muessen unterschiedliche Farbe haben"
+            )
+        )
+
+        @JvmStatic
+        fun ungueltigeSetsUnterschiedlicheZahl() = listOf(
+            // 4 Steine - unterschiedliche Zahlen
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Eins),
+                    Stein(Farbe.Rot, Zahl.Dreizehn),
+                    Stein(Farbe.Blau, Zahl.Dreizehn),
+                    Stein(Farbe.Schwarz, Zahl.Dreizehn)
+                ),
+                "Alle Steine muessen dieselbe Zahl haben"
+            ),
+            // 3 Steine - unterschiedliche Zahlen
+            Pair(
+                mutableListOf(
+                    Stein(Farbe.Orange, Zahl.Eins),
+                    Stein(Farbe.Rot, Zahl.Dreizehn),
+                    Stein(Farbe.Blau, Zahl.Dreizehn)
+                ),
+                "Alle Steine muessen dieselbe Zahl haben"
+            )
+        )
     }
 
-    @Test
-    fun `ungueltige Set mit vier Steine, selbe Zahl (Eins) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Rot, Zahl.Eins),
-            Stein(Farbe.Rot, Zahl.Eins),
-            Stein(Farbe.Blau, Zahl.Eins),
-            Stein(Farbe.Schwarz, Zahl.Eins)
-        )
-        //when
-        val sets = Sets(set)
-
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            sets.istGueltig()
-        }
-        assertThat(exception.message).contains("Alle Steine muessen unterschiedliche Farbe haben")
-    }
-
-    @Test
-    fun `gueltige Set mit vier Steine, selbe Zahl (Sechs) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Sechs),
-            Stein(Farbe.Rot, Zahl.Sechs),
-            Stein(Farbe.Blau, Zahl.Sechs),
-            Stein(Farbe.Schwarz, Zahl.Sechs)
-        )
-        //when
+    @ParameterizedTest
+    @MethodSource("gueltigeSets3Steine")
+    fun `gueltige Sets mit 3 Steinen werfen keine Exception`(
+        set: MutableList<Stein>
+    ) {
+        // when
         val sets = Sets(set)
         sets.istGueltig()
 
-        //then
+        // then
         assertThat(sets.setReadOnly).isEqualTo(set)
     }
 
-    @Test
-    fun `ungueltige Set mit vier Steine, selbe Zahl (Sechs) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Sechs),
-            Stein(Farbe.Orange, Zahl.Sechs),
-            Stein(Farbe.Blau, Zahl.Sechs),
-            Stein(Farbe.Schwarz, Zahl.Sechs)
-        )
-        //when
+    @ParameterizedTest
+    @MethodSource("gueltigeSets4Steine")
+    fun `gueltige Sets mit 4 Steinen werfen keine Exception`(
+        set: MutableList<Stein>
+    ) {
+        // when
         val sets = Sets(set)
 
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            sets.istGueltig()
-        }
-        assertThat(exception.message).contains("Alle Steine muessen unterschiedliche Farbe haben")
-    }
-
-    @Test
-    fun `gueltige Set mit vier Steine, selbe Zahl (Dreizehn) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Dreizehn),
-            Stein(Farbe.Rot, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn),
-            Stein(Farbe.Schwarz, Zahl.Dreizehn)
-        )
-        //when
-        val sets = Sets(set)
-        sets.istGueltig()
-
-        //then
+        // then
         assertThat(sets.setReadOnly).isEqualTo(set)
     }
 
-    @Test
-    fun `ungueltige Set mit vier Steine, selbe Zahl (Dreizehn) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn),
-            Stein(Farbe.Schwarz, Zahl.Dreizehn)
-        )
-        //when
-        val sets = Sets(set)
+    @ParameterizedTest
+    @MethodSource("ungueltigeSetsGleicheFarbe")
+    fun `ungueltige Sets mit gleichen Farben werfen Exception`(
+        testfall: Pair<MutableList<Stein>, String>
+    ) {
+        // when
+        val sets = Sets(testfall.first)
 
-        //then
+        // then
         val exception = assertThrows<IllegalArgumentException> {
             sets.istGueltig()
         }
-        assertThat(exception.message).contains("Alle Steine muessen unterschiedliche Farbe haben")
+        assertThat(exception.message).contains(testfall.second)
     }
 
-    @Test
-    fun `ungueltige Set mit vier Steine, unterschiedlichen Zahl und unterschiedlichen Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Rot, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn),
-            Stein(Farbe.Schwarz, Zahl.Dreizehn)
-        )
-        //when
-        val sets = Sets(set)
+    @ParameterizedTest
+    @MethodSource("ungueltigeSetsUnterschiedlicheZahl")
+    fun `ungueltige Sets mit unterschiedlichen Zahlen werfen Exception`(
+        testfall: Pair<MutableList<Stein>, String>
+    ) {
+        // when
+        val sets = Sets(testfall.first)
 
-        //then
+        // then
         val exception = assertThrows<IllegalArgumentException> {
             sets.istGueltig()
         }
-        assertThat(exception.message).contains("Alle Steine muessen dieselbe Zahl haben")
-    }
-
-    @Test
-    fun `gueltige Set mit drei Steine, selbe Zahl (Eins) und unterschiedliche Farben`() {
-        // given
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Rot, Zahl.Eins),
-            Stein(Farbe.Blau, Zahl.Eins),
-        )
-        //when
-        val sets = Sets(set)
-        sets.istGueltig()
-
-        //then
-        assertThat(sets.setReadOnly).isEqualTo(set)
-    }
-
-    @Test
-    fun `ungueltige Set mit drei Steine, selbe Zahl (Eins) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Blau, Zahl.Eins),
-        )
-        //when
-        val sets = Sets(set)
-
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            sets.istGueltig()
-        }
-        assertThat(exception.message).contains("Alle Steine muessen unterschiedliche Farbe haben")
-    }
-
-    @Test
-    fun `gueltige Set mit drei Steine, selbe Zahl (Sechs) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Sechs),
-            Stein(Farbe.Blau, Zahl.Sechs),
-            Stein(Farbe.Schwarz, Zahl.Sechs)
-        )
-        //when
-        val sets = Sets(set)
-        sets.istGueltig()
-
-        //then
-        assertThat(sets.setReadOnly).isEqualTo(set)
-    }
-
-    @Test
-    fun `ungueltige Set mit drei Steine, selbe Zahl (Sechs) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Sechs),
-            Stein(Farbe.Schwarz, Zahl.Sechs),
-            Stein(Farbe.Schwarz, Zahl.Sechs)
-        )
-        //when
-        val sets = Sets(set)
-
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            sets.istGueltig()
-        }
-        assertThat(exception.message).contains("Alle Steine muessen unterschiedliche Farbe haben")
-    }
-
-    @Test
-    fun `gueltige Set mit drei Steine, selbe Zahl (Dreizehn) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Rot, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn),
-            Stein(Farbe.Schwarz, Zahl.Dreizehn)
-        )
-        //when
-        val sets = Sets(set)
-        sets.istGueltig()
-
-        //then
-        assertThat(sets.setReadOnly).isEqualTo(set)
-    }
-
-    @Test
-    fun `ungueltige Set mit drei Steine, selbe Zahl (Dreizehn) und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Rot, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn)
-        )
-        //when
-        val sets = Sets(set)
-
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            sets.istGueltig()
-        }
-        assertThat(exception.message).contains("Alle Steine muessen unterschiedliche Farbe haben")
-    }
-
-    @Test
-    fun `ungueltige Set mit drei Steine, unterschiedlichen Zahl und unterschiedliche Farben`() {
-        val set = mutableListOf(
-            Stein(Farbe.Orange, Zahl.Eins),
-            Stein(Farbe.Rot, Zahl.Dreizehn),
-            Stein(Farbe.Blau, Zahl.Dreizehn)
-        )
-        //when
-        val sets = Sets(set)
-
-        //then
-        val exception = assertThrows<IllegalArgumentException> {
-            sets.istGueltig()
-        }
-        assertThat(exception.message).contains("Alle Steine muessen dieselbe Zahl haben")
+        assertThat(exception.message).contains(testfall.second)
     }
 }

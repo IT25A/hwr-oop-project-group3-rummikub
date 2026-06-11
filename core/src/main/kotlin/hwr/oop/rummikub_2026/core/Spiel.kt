11e75cbbabc42e859692e619e4f30870d3e1ee06
Smilla Finschow <smilla.finschow@sap.com>
2026-06-11 15:03:51 +0200

    Fertiges Spiel

--- core/src/main/kotlin/hwr/oop/rummikub_2026/core/Spiel.kt
+++ core/src/main/kotlin/hwr/oop/rummikub_2026/core/Spiel.kt
@@ -2,38 +2,69 @@ package hwr.oop.rummikub_2026.core
 
 import java.io.InvalidObjectException
 
+
 data class Spiel(
     val aktivSpieler: Spieler,
     val beutel: List<Stein>,
-    val spielerBretter:
-    Map<Spieler, List<Stein>>
-) {
+    val spielerBretter: Map<Spieler, List<Stein>>,
+    val alleSteine: List<Kombinationen>
+)
+{
+
     fun ziehen(spieler: Spieler): Spiel {
 
         if (spieler != aktivSpieler) {
             throw InvalidObjectException("Spieler ist nicht an der Reihe!")
         }
-
-
         if (beutel.isEmpty()) {
             throw IllegalStateException("Der Beutel ist leer!")
         }
-
-
         val gezogenerStein = beutel.first()
         val neuerBeutel = beutel - gezogenerStein
-
 
         val aktuelleSteine = spielerBretter[aktivSpieler] ?: aktivSpieler.boardReadOnly
         val neueSteine = aktuelleSteine + gezogenerStein
 
-
         val neueBretterMap = spielerBretter + (aktivSpieler to neueSteine)
-
 
         return this.copy(
             beutel = neuerBeutel,
             spielerBretter = neueBretterMap
+        )
+    }
+
+    fun legen(spieler: Spieler,
+              kombination: Kombinationen,
+              neuStein: List<Stein>,
+              alteKombination: Kombinationen? = null
+    ): Spiel {
+
+        if (spieler != aktivSpieler) {
+            throw InvalidObjectException("Spieler ist nicht an der Reihe!")
+        }
+
+        val brett = spielerBretter[spieler]!!
+        if (!brett.containsAll(neuStein)) {
+            throw InvalidObjectException("Du hast diesen Stein nicht!")
+        }
+        if (neuStein.isEmpty()) {
+            throw InvalidObjectException("Keine Steine ausgewählt!")
+        }
+
+        kombination.istGueltig()
+
+        val gesamtSteine = if (alteKombination != null) {
+            // Stein wird an Kombi angelegt
+            alleSteine - alteKombination + kombination
+        } else {
+            // Neue Kombination wird gelegt
+            alleSteine + kombination
+        }
+        val neuBrett = spielerBretter + (spieler to (brett - neuStein.toSet()))
+
+        return this.copy(
+            alleSteine = gesamtSteine,
+            spielerBretter = neuBrett
         )
     }
 }
--- core/src/test/kotlin/hwr/oop/rummikub_2026/core/SpielTest.kt
+++ core/src/test/kotlin/hwr/oop/rummikub_2026/core/SpielTest.kt
@@ -1,9 +1,12 @@
 package hwr.oop.rummikub_2026.core
 
+import hwr.oop.rummikub_2026.core.Kombinationen
+import hwr.oop.rummikub_2026.core.Stein
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.io.InvalidObjectException
 import org.junit.jupiter.api.assertThrows
+import kotlin.collections.List
 
 class SpielTest {
 
@@ -33,12 +36,17 @@ class SpielTest {
         val spieler1 = Spieler("Luxi-Taxi", "1", steine14.toMutableList())
         val spieler2 = Spieler("Smilla", "2", steine14.toMutableList())
 
+        val startBretter = mapOf(
+            spieler1 to spieler1.boardReadOnly,
+            spieler2 to spieler2.boardReadOnly
+        )
+
         val spiel = Spiel(
             aktivSpieler = spieler1,
             beutel = emptyList(),
-            spielerBretter = emptyMap()
+            spielerBretter = startBretter,
+            alleSteine = emptyList()
         )
-
 
         assertThrows<InvalidObjectException> {
             spiel.ziehen(spieler2)
@@ -50,19 +58,17 @@ class SpielTest {
 
         val spieler1 = Spieler("Luxi-Taxi", "1", steine14.toMutableList())
         val startBeutel = listOf(steinImBeutel1, steinImBeutel2)
-
-
         val startBretter = mapOf(spieler1 to spieler1.boardReadOnly)
 
-        val spiel = Spiel(aktivSpieler = spieler1, beutel = startBeutel, spielerBretter = startBretter)
-
+        val spiel = Spiel(
+            aktivSpieler = spieler1,
+            beutel = startBeutel,
+            spielerBretter = startBretter,
+            alleSteine = emptyList()
+        )
 
         val neuesSpiel = spiel.ziehen(spieler1)
-
-
-
         val endBrettDesSpielers = neuesSpiel.spielerBretter[spieler1]!!
-
 
         assertThat(endBrettDesSpielers).hasSize(15)
         assertThat(endBrettDesSpielers).contains(steinImBeutel1)
@@ -79,13 +85,194 @@ class SpielTest {
         val spiel = Spiel(
             aktivSpieler = spieler1,
             beutel = emptyList(),
-            spielerBretter = startBretter
+            spielerBretter = startBretter,
+            alleSteine = emptyList()
         )
-
 
         val exception = assertThrows<IllegalStateException> {
             spiel.ziehen(spieler1)
         }
         assertThat(exception.message).isEqualTo("Der Beutel ist leer!")
+    }
+    @Test
+    fun `Spieler spielt nicht vorhandenen Stein`(){
+
+        val spieler1 = Spieler("Luxi-Taxi", "1", steine14.toMutableList())
+        val startBretter = mapOf(
+            spieler1 to listOf(
+                Stein(Farbe.Blau, Zahl.Fuenf),
+                Stein(Farbe.Blau, Zahl.Sechs),
+                Stein(Farbe.Blau, Zahl.Acht),
+            )
+        )
+        val spiel = Spiel(
+            aktivSpieler = spieler1,
+            beutel = emptyList(),
+            spielerBretter = startBretter,
+            alleSteine = emptyList()
+        )
+
+        val kombination = Folge(
+            mutableListOf(
+                Stein(Farbe.Blau, Zahl.Fuenf),
+                Stein(Farbe.Blau, Zahl.Sechs),
+                Stein(Farbe.Blau, Zahl.Sieben)
+            )
+        )
+
+        val exception = assertThrows<InvalidObjectException> {
+            spiel.legen(
+                spieler1,
+                kombination,
+                listOf(
+                    Stein(Farbe.Blau, Zahl.Fuenf),
+                    Stein(Farbe.Blau, Zahl.Sechs),
+                    Stein(Farbe.Blau, Zahl.Sieben)  // <- Hat er nicht
+                )
+            )
+        }
+        assertThat(exception.message).isEqualTo("Du hast diesen Stein nicht!")
+    }
+
+    @Test
+    fun `Spieler legt nichts`(){
+        val spieler1 = Spieler("Luxi-Taxi", "1", steine14.toMutableList())
+        val startBretter = mapOf(
+            spieler1 to listOf(
+                Stein(Farbe.Blau, Zahl.Fuenf),
+                Stein(Farbe.Blau, Zahl.Sechs),
+                Stein(Farbe.Blau, Zahl.Acht),
+            )
+        )
+        val spiel = Spiel(
+            aktivSpieler = spieler1,
+            beutel = emptyList(),
+            spielerBretter = startBretter,
+            alleSteine = emptyList()
+        )
+
+        val kombination = Folge(
+            mutableListOf(
+            )
+        )
+
+
+        val exception = assertThrows<InvalidObjectException> {
+            spiel.legen(
+                spieler1,
+                kombination,
+                listOf()
+            )
+        }
+        assertThat(exception.message).isEqualTo("Keine Steine ausgewählt!")
+    }
+
+    @Test
+    fun `ungueltiger Spieler darf nicht legen`() {
+
+        val spieler1 = Spieler("Luxi-Taxi", "1", steine14.toMutableList())
+        val spieler2 = Spieler("Smilla", "2", steine14.toMutableList())
+
+        val startBretter = mapOf(
+            spieler1 to spieler1.boardReadOnly,
+            spieler2 to spieler2.boardReadOnly
+        )
+
+        val spiel = Spiel(
+            aktivSpieler = spieler1,
+            beutel = emptyList(),
+            spielerBretter = startBretter,
+            alleSteine = emptyList()
+        )
+
+        val kombination = Folge(
+            mutableListOf(
+                Stein(Farbe.Blau, Zahl.Fuenf),
+                Stein(Farbe.Blau, Zahl.Sechs),
+                Stein(Farbe.Blau, Zahl.Sieben)
+            )
+        )
+
+        val exception = assertThrows<InvalidObjectException> {
+            spiel.legen(
+                spieler2,
+                kombination,
+                listOf()
+            )
+        }
+        assertThat(exception.message).isEqualTo("Spieler ist nicht an der Reihe!")
+    }
+
+    @Test
+    fun `Spieler legt neue gueltige Folge`() {
+        val spieler1 = Spieler("Luxi-Taxi", "1", steine14.toMutableList())
+        val stein1 = Stein(Farbe.Blau, Zahl.Fuenf)
+        val stein2 = Stein(Farbe.Blau, Zahl.Sechs)
+        val stein3 = Stein(Farbe.Blau, Zahl.Sieben)
+
+        val startBretter = mapOf(
+            spieler1 to listOf(stein1, stein2, stein3)
+        )
+
+        val spiel = Spiel(
+            aktivSpieler = spieler1,
+            beutel = emptyList(),
+            spielerBretter = startBretter,
+            alleSteine = emptyList()
+        )
+
+        val kombination = Folge(
+            mutableListOf(stein1, stein2, stein3)
+        )
+
+        val neuesSpiel = spiel.legen(
+            spieler1,
+            kombination,
+            listOf(stein1, stein2, stein3)
+        )
+
+        assertThat(neuesSpiel.alleSteine).hasSize(1)
+        assertThat(neuesSpiel.alleSteine).contains(kombination)
+        assertThat(neuesSpiel.spielerBretter[spieler1]).isEmpty()
+    }
+
+    @Test
+    fun `Spieler legt Stein an bestehende Kombination an`() {
+        val spieler1 = Spieler("Luxi-Taxi", "1", steine14.toMutableList())
+        val stein1 = Stein(Farbe.Rot, Zahl.Fuenf)
+        val stein2 = Stein(Farbe.Rot, Zahl.Sechs)
+        val stein3 = Stein(Farbe.Rot, Zahl.Sieben)
+        val neuerStein = Stein(Farbe.Rot, Zahl.Acht)
+
+        val alteKombination = Folge(
+            mutableListOf(stein1, stein2, stein3)
+        )
+
+        val startBretter = mapOf(
+            spieler1 to listOf(neuerStein)
+        )
+
+        val spiel = Spiel(
+            aktivSpieler = spieler1,
+            beutel = emptyList(),
+            spielerBretter = startBretter,
+            alleSteine = listOf(alteKombination)
+        )
+
+        val neueKombination = Folge(
+            mutableListOf(stein1, stein2, stein3, neuerStein)
+        )
+
+        val neuesSpiel = spiel.legen(
+            spieler1,
+            neueKombination,
+            listOf(neuerStein),
+            alteKombination
+        )
+
+        assertThat(neuesSpiel.alleSteine).hasSize(1)
+        assertThat(neuesSpiel.alleSteine).contains(neueKombination)
+        assertThat(neuesSpiel.alleSteine).doesNotContain(alteKombination)
+        assertThat(neuesSpiel.spielerBretter[spieler1]).isEmpty()
     }
 }

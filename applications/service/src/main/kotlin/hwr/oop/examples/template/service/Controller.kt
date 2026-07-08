296adab31aab3e565a5f37a496d51d10ffbd94db
Gabriela Siezar Castillo <s_siezarcastillo25@stud.hwr-berlin.de>
2026-07-08 22:52:27 +0200

    Arbeiten am Applications (Fehlerhaft)

--- applications/service/src/main/kotlin/hwr/oop/examples/template/service/Controller.kt
+++ applications/service/src/main/kotlin/hwr/oop/examples/template/service/Controller.kt
@@ -1,5 +1,9 @@
 package hwr.oop.examples.template.service
 
+import hwr.oop.rummikub_2026.adapters.`in`.NewGameUseCase
+import hwr.oop.rummikub_2026.adapters.`in`.PlayTileUseCase
+import hwr.oop.rummikub_2026.adapters.`in`.LoadGameByIdQuery
+
 import hwr.oop.examples.template.service.api.GameActionApi
 import hwr.oop.examples.template.service.api.GameApi
 import hwr.oop.examples.template.service.model.*
@@ -8,27 +12,49 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class Controller : GameApi, GameActionApi {
+class Controller(
+	private val newGameUseCase: NewGameUseCase,
+	private val playTileUseCase: PlayTileUseCase,
+	private val loadGameByIdQuery: LoadGameByIdQuery,
+) : GameApi, GameActionApi {
+	
 	override fun getGame(gameId: String?): ResponseEntity<GameState> {
-		TODO("Not yet implemented")
+		require(gameId != null) { "Game ID is null" }
+		val loadedGame = loadGameByIdQuery.SpielLadenById(gameId)
+		val response = with(ResponseMapper) {
+			loadedGame.asGameResponse()
+		}
+		return ResponseEntity.ok(response)
 	}
 	
 	override fun startGame(startGameRequest: @Valid StartGameRequest?): ResponseEntity<GameCreatedResponse> {
-		TODO("Not yet implemented")
+		require(startGameRequest != null) { "required request body (CreateGameRequest) was null" }
+		val command = with(RequestMapper) {
+			startGameRequest.asCommand()
+		}
+		newGameUseCase.startGame(command)
+		val response = GameCreatedResponse(command.gameId)
+		return ResponseEntity.status(201).body(response)
 	}
 	
 	override fun drawTile(
 		gameId: String?,
 		drawTileRequest: @Valid DrawTileRequest?,
 	): ResponseEntity<GameState>? {
-		TODO("Not yet implemented")
+		//TODO("Not yet implemented")
 	}
 	
 	override fun playTiles(
 		gameId: String?,
 		playTilesRequest: @Valid PlayTilesRequest?,
 	): ResponseEntity<GameState> {
-		TODO("Not yet implemented")
+		require(gameId != null) { "Game ID is null" }
+		require(playTilesRequest != null) { "required request body (PlayTilesRequest) was null" }
+		val command: PlayTileUseCase.Command = with(RequestMapper) {
+			playTilesRequest.asCommand(gameId)
+		}
+		playTileUseCase.playAction(command)
+		return getGame(gameId)
 	}
 	
 }
---
+++ core/src/main/kotlin/hwr/oop/rummikub_2026/adapters/in/PlayTileUseCase.kt
@@ -0,0 +1,26 @@
+package hwr.oop.examples.doppelkopf_2026.adapters.`in`
+
+import hwr.oop.examples.doppelkopf_2026.core.GameId
+import hwr.oop.examples.doppelkopf_2026.ports.out.LoadGameByIdPort
+import hwr.oop.examples.doppelkopf_2026.ports.out.SaveGamePort
+
+class PlayTileUseCase(
+	private val loadGameByIdPort: LoadGameByIdPort,
+	private val saveGamePort: SaveGamePort,
+) {
+	
+	fun playAction(command: Command) {
+		val gameId = GameId(command.gameId)
+		val loadedGame = loadGameByIdPort.loadByid(gameId)
+		val updatedGame = TODO("domain logic on game")
+		saveGamePort.save(updatedGame)
+	}
+	
+	data class Command(
+		val gameId: String,
+		val player: String,
+		val suit: String,
+		val rank: String,
+	)
+	
+}
